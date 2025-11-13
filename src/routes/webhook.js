419f5adc5b4e4b6b12be6c899572fef76c2e045f
Samarth622 <guptasamarth709@gmail.com>
2025-11-13 13:33:10 +0530

    webhook test 2

--- src/routes/webhook.js
+++ src/routes/webhook.js
@@ -5,6 +5,10 @@ router.post("/", (req, res) => {
   const event = req.headers["x-github-event"];
   const payload = req.body;
 
+  console.log("🚀 Headers: ", req.headers);
+
+  console.log("📬 Webhook received: ", payload);
+
   console.log(`📦 Received GitHub Event: ${event}`);
   console.log(`🧑 Author: ${payload.head_commit?.author?.name}`);
   console.log(`📄 Message: ${payload.head_commit?.message}`);
